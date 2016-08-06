5fbe9fbf69a1e68c2a24b279d80997d93a03c209
Mike King <mikkelking@hotmail.com>
2016-08-07 00:40:34 +1000

    Cloned from demo repo, added some debug

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Basic WebRTC tutorial using Meteor.js and PeerJS
+
+See the blog post:
+
+http://www.mhurwi.com/a-basic-webrtc-tutorial-using-meteor-peerjs/.
---
+++ webrtc-peerjs.js
@@ -0,0 +1,112 @@
+// get audio/video
+var enableVideo = function () {
+  navigator.getUserMedia({audio:true, video: true}, function (stream) {
+  console.log("Connecting media to stream");
+  //display video
+  var video = document.getElementById("myVideo");
+  video.src = URL.createObjectURL(stream);
+  window.localStream = stream;
+}, function (error) { console.log(error); }
+);
+}
+
+if (Meteor.isClient) {
+  Template.hello.events({
+    "click #makeCall": function () {
+      var user = this;
+      enableVideo();
+      var outgoingCall = peer.call(user.profile.peerId, window.localStream);
+      window.currentCall = outgoingCall;
+      outgoingCall.on('stream', function (remoteStream) {
+        window.remoteStream = remoteStream;
+        var video = document.getElementById("theirVideo")
+        video.src = URL.createObjectURL(remoteStream);
+      });
+    },
+    "click #endCall": function () {
+      window.currentCall.close();
+    }
+  });
+
+  Template.hello.helpers({
+    users: function () {
+      // exclude the currentUser
+      var userIds = Presences.find().map(function(presence) {return presence.userId;});
+      return Meteor.users.find({_id: {$in: userIds, $ne: Meteor.userId()}});
+    }
+  });
+
+  Template.hello.onCreated(function () {
+    Meteor.subscribe("presences");
+    Meteor.subscribe("users");
+
+    console.log("Setting up own presence");
+    window.peer = new Peer({
+      key: '3hlis32874fe0zfr',  // change this key
+      debug: 3,
+      config: {'iceServers': [
+        { url: 'stun:stun.l.google.com:19302' },
+        { url: 'stun:stun1.l.google.com:19302' },
+      ]}
+    });
+
+    // This event: remote peer receives a call
+    peer.on('open', function () {
+      console.log("Connected to PeerJS Server");
+      $('#myPeerId').text(peer.id);
+      // update the current user's profile
+      Meteor.users.update({_id: Meteor.userId()}, {
+        $set: {
+          profile: { peerId: peer.id}
+        }
+      });
+    });
+
+    peer.on('connection', function () {
+      console.log("Connection from remote");
+    });
+
+    peer.on('close', function () {
+      console.log("close - peer destroyed");
+    });
+
+    peer.on('disconnected', function () {
+      console.log("disconnected from remote");
+    });
+
+    peer.on('error', function (err) {
+      console.log("Error from remote: "+err);
+    });
+
+    // This event: remote peer receives a call
+    peer.on('call', function (incomingCall) {
+      console.log("Making a call");
+      window.currentCall = incomingCall;
+      incomingCall.answer(window.localStream);
+      incomingCall.on('stream', function (remoteStream) {
+        window.remoteStream = remoteStream;
+        var video = document.getElementById("theirVideo")
+        video.src = URL.createObjectURL(remoteStream);
+      });
+    });
+
+    console.log("Setting up user media");
+
+    navigator.getUserMedia = ( navigator.getUserMedia ||
+                      navigator.webkitGetUserMedia ||
+                      navigator.mozGetUserMedia ||
+                      navigator.msGetUserMedia );
+
+
+  });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.publish('presences', function() {
+    return Presences.find({}, { userId: true });
+  });
+  Meteor.publish("users", function () {
+    return Meteor.users.find({}, {fields: {"profile.peerId": true, "emails.address": true} });
+  });
+}
